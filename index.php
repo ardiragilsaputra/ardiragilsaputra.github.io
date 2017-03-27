c7330ee9598429f6b4a86ce3601a1f7ef1866453
Ardi Ragil Saputra <ardi.rs@gmail.com>
2017-03-27 22:56:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,84 @@
-<?php
-function header(){
-  echo 'CodeinTerm ';
-}
-function id(){
-  echo 'Nama      = ' $nama ;
-  echo 'Identitas = ' $jk   ;
-  echo 'Born      = ' $born ;
-}
-header();
-id('Ardi Ragil Saputra','Mahasiswa',30 'june' 1998'); 
-?>
+!DOCTYPE html
+<html>
+<head>
+  <title>Ardi Ragil Saputra</title>
+  <style>
+    h2{
+      color="deep blue";
+      background-color="grey";
+    };
+    h1{
+      color="blue";
+      background-color="red;
+    };
+    li{
+      color="green"
+    };
+    table{
+    border = 0 ;
+    color ="orange";
+    };
+  </style>
+</head>
+<body>
+  <h1> Ardi Ragil Saputra</h1>
+  <span align-text="center"> Make a Real Coding </span>
+  <br>
+  <hr>
+  <h2>Identitas</h2>
+  <hr>
+  <ol>
+    <li> Lahir  : Pujodadi, 30 june 1998 </li>
+    <li> Gender : Laki- Laki </li>
+    <li> Karakter : Kreatif </li>
+  </ol>
+  <h2>Riwayat<h2>
+    <hr>
+    <h3>Pendidikan</h3>
+    <ol>
+    <li> TK Dharma Wanita <li>  
+    <li> SD Negri 1 Ppujodadi </li>
+    <li> SMP Negri 1 Trimurjo  </li>
+    <li> Universitas Lampung </li>
+    </ol>
+    <h3>Belajar bahasa</h3>
+    <ol>
+      <li>Bahasa Indonesia</li>
+      <li>Bahasa Lampung </li>
+      <li>Bahasa Inggris </li>
+      <li>Bahasa Jepang </li>
+      <li>Bahasa Pemrograman  </li>
+      <ul>
+        <li>Bahasa Html  </li>
+        <li>Bahasa CSS </li>
+        <li>Bahasa PHP </li>
+        <li>Bahasa Python </li>
+        <li>Bahasa C++ </li>
+        <li>Bahasa Java </li>
+        <li>Bahasa JavaScript </li>
+      </ul>
+    </ol>
+ <br>
+    <h2> Hobi </h2>
+    <hr>
+    <ul> 
+      <li>Main Komputer</li>
+      <li>Tentang Jepang </li>
+      <li>otak atik website </li>
+    </ul>
+  <br>
+    <h2> Site</h2>
+    <table>
+      <tr><td>Facebook</td><td>:<td><td>Ardi R Saputra</td></tr>
+      <tr><td>LINE</td><td>:</td><td>ardiragils</td></tr>
+      <tr><td>BBM</td><td>:</td><td>D0E03128</td></tr>
+      <tr><td>Twitter</td><td>:</td><td>@ardi_rsaputra</td></tr>
+      <tr><td>Whats Up</td><td>:</td><td>+6282280699457</td></tr>
+      <tr><td>Slack</td><td>:</td><td>ardiragilsaputra</td></tr>
+      <tr><td>Instagram</td><td>:</td><td>ardiragilsaputra</td></tr>
+      <tr><td>Gmail</td><td>:</td><td>ardi.rs@gmail.com</td></tr>
+    </table>
+    <hr>
+    <br>
+  </body>
+</html>
