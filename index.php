942594d2393c537aafa8bae20b41842007561890
Ardi Ragil Saputra <ardi.rs@gmail.com>
2017-03-27 21:46:24 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+function header(){
+  echo 'CodeinTerm ';
+}
+function id(){
+  echo 'Nama      = ' $nama ;
+  echo 'Identitas = ' $jk   ;
+  echo 'Born      = ' $born ;
+}
+header();
+id('Ardi Ragil Saputra','Mahasiswa',30 'june' 1998'); 
+?>
